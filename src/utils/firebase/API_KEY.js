4889dc2b3f4c9e32bd136e68ec0cbdf1d3e3a3c9
yoshuah809 <91335755+yoshuah809@users.noreply.github.com>
2022-04-14 13:38:27 -0500

    performed firebase initialization

---
+++ src/utils/firebase/API_KEY.js
@@ -0,0 +1,1 @@
+export const API_KEY = "AIzaSyDSG1JH9XLumntwSCJFzD6WpTVbZpq2sX0";
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+} from "firebase/auth";
+import { API_KEY } from "./API_KEY";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: "crwn-clothingstoresdb.firebaseapp.com",
+  projectId: "crwn-clothingstoresdb",
+  storageBucket: "crwn-clothingstoresdb.appspot.com",
+  messagingSenderId: "377114080598",
+  appId: "1:377114080598:web:ae6cd855f17c590b1c12f7"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  propt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
