04d6950787dfe99b5a481664a8861ce286c191f1
Josue <yoshuah809@gmail.com>
2022-05-25 07:17:31 -0500

    created category component

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,24 @@
+import "./category.styles.scss";
+import { useParams } from "react-router-dom";
+import { useContext, useState, useEffect } from "react";
+import { CategoriesContext } from "../../contexts/categories.context";
+import { ProductCard } from "../../components/product-card/product-card.component";
+
+export const Category = () => {
+	const { category } = useParams();
+	const { categoriesMap } = useContext(CategoriesContext);
+	const [products, setProducts] = useState(categoriesMap[category]);
+
+	useEffect(() => {
+		setProducts(categoriesMap[category]);
+	}, [category, categoriesMap]);
+
+	return (
+		<div className="category-container">
+			{products &&
+				products.map((product) => (
+					<ProductCard key={product.id} product={product} />
+				))}
+		</div>
+	);
+};
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,20 +1,14 @@
-import { useContext, Fragment } from "react";
-import { CategoryPreview } from "../../components/category-preview/category-preview.component";
-import { ProductCard } from "../../components/product-card/product-card.component";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { Routes, Route } from "react-router-dom";
+import { CategoriesPreview } from "../categories-preview/categories.preview.component";
+import { Category } from "../category/category.component";
 
 import "./shop.styles.scss";
 
 export const Shop = () => {
-	const { categoriesMap } = useContext(CategoriesContext);
 	return (
-		<div className="shop-container">
-			{Object.keys(categoriesMap).map((title) => {
-				const products = categoriesMap[title];
-				return (
-					<CategoryPreview key={title} title={title} products={products} />
-				);
-			})}
-		</div>
+		<Routes>
+			<Route index element={<CategoriesPreview />} />
+			<Route path=":category" element={<Category />} />
+		</Routes>
 	);
 };
